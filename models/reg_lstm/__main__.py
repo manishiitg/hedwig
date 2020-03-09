73ca3e479bb1a1fb51a5ea90299fbc7d11c3db2d
recruititserver <recruitserver>
2020-03-09 23:01:03 +0530

    fix

--- models/reg_lstm/__main__.py
+++ models/reg_lstm/__main__.py
@@ -33,14 +33,14 @@ class UnknownWordVecCache(object):
 
 
 def get_logger():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.DEBUG)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    # ch = logging.StreamHandler()
+    # ch.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter('%(levelname)s - %(message)s')
+    # ch.setFormatter(formatter)
+    # logger.addHandler(ch)
 
     return logger
 
